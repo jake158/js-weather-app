bddc559018102fb1b669555e05ec8203a3c28579
freeram <kulikovpavel2801@gmail.com>
2024-08-01 10:09:43 -0600

    Add search field for entering location

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import 'modern-normalize';
 import './style.css';
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () =>
+    new SearchController(
+      document.querySelector('#search-location-form'),
+      document.querySelector('#search')
+    )
+);
+
+class SearchController {
+  constructor(form, searchInput) {
+    this.form = form;
+    this.input = searchInput;
+    this.weather = new WeatherAdapter();
+
+    this.form.addEventListener('submit', (event) => this.onUserSearch(event));
+  }
+
+  onUserSearch(event) {
+    event.preventDefault();
+    const location = this.input.value;
+    const data = this.weather.getLocationData(location);
+  }
+}
+
+class WeatherAdapter {
+  constructor() {}
+
+  getLocationData(locationString) {
+    console.log(locationString);
+    return null;
+  }
+}
