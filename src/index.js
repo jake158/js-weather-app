61f8d16fd1af284acdd312fe782d3f753dfd7e70
freeram <kulikovpavel2801@gmail.com>
2024-08-01 12:15:12 -0600

    Add API data standardization

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import VcWeatherAPI from './visualCrossingAPI';
 document.addEventListener(
   'DOMContentLoaded',
   () =>
-    new SearchController(
+    new ScreenController(
       document.querySelector('#search-location-form'),
       document.querySelector('#search')
     )
 );
 
-class SearchController {
+class ScreenController {
   constructor(form, searchInput) {
     this.form = form;
     this.input = searchInput;
@@ -25,10 +25,18 @@ class SearchController {
     const location = this.input.value;
     try {
       const data = await this.weather.getLocationData(location);
-      console.log(data);
+      this.initializeCards(data);
     } catch (error) {
-      console.log(error.message);
+      this.showError(error.message);
     }
+  }
+
+  initializeCards(data) {
+    console.log(data);
+  }
+
+  showError(message) {
+    console.error(message);
   }
 }
 
@@ -37,8 +45,33 @@ class WeatherAdapter {
     this.api = new VcWeatherAPI();
   }
 
+  #standardizeData(data) {
+    /*
+    Returns an object with the following attributes:
+
+    1. address: string
+
+    2. data: Array of 5 objects
+    Each object represents a day, ordered (index 0 - current, index 1 - next), and must have the following attributes:
+    temp: float, tempmin: float, tempmax: float, humidity: float, windspeed: float, conditions: string
+    icon: in set {'snow', 'rain', 'fog', 'wind', 'cloudy', 'partly-cloudy-day', 'partly-cloudy-night', 'clear-day', 'clear-night'}
+    */
+    data.days['0'].temp = data.currentConditions.temp;
+
+    return {
+      address: data.address,
+      data: [
+        data.days['0'],
+        data.days['1'],
+        data.days['2'],
+        data.days['3'],
+        data.days['4'],
+      ],
+    };
+  }
+
   async getLocationData(locationString) {
     const data = await this.api.getLocationData(locationString);
-    return data;
+    return this.#standardizeData(data);
   }
 }
